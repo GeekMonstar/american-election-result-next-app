18436091f52bc38a87ca44482029a243e336ede3
Juan Vianney NM <juanvianneynm@gmail.com>
2024-11-10 18:45:39 +0100

    fix bug

--- app/action.ts
+++ app/action.ts
@@ -4,9 +4,10 @@ import axios from "axios";
 import { Result } from "./page";
 
 export async function getResult(){
+    const root = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000";
     const request = await axios({
         method: "GET",
-        url: "/data/results.json"
+        url: `${root}/data/results.json`
     })
     const response = request.data.election as Result;
     return response;
